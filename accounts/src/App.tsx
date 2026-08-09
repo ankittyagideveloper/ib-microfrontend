9fcc873b3e3f23c0e278f2232f191693d91093c9
Ankit Tyagi <ankittyagideveloper@email.com>
2026-08-09 08:50:25 +0530

    feat(accounts): add accounts microfrontend with webpack and React setup

---
+++ accounts/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <>Account Repo</>
+}
+
+export default App;
---
+++ accounts/webpack.dev.js
@@ -0,0 +1,3 @@
+import {createHostDevelopmentConfig} from 'app-config'
+
+export default createHostDevelopmentConfig(process.env.MODULE_NAME,{})
